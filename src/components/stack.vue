635aa3bf4e9509f11a4d72e7063f4c3495ab4631
biranastasiia <biryukstasiya@gmail.com>
2021-06-04 21:08:49 +0300

    create new app design

--- src/components/stack.vue
+++ src/components/stack.vue
@@ -1,46 +1,56 @@
 <template>
-  <div class="container">
-    <div class="stack">
-      <h1 class="stack__title">Russian roulette</h1>
-      <div class="stack__content">
-        <div class="stack__player">
-          <div class="stack__player-title">BOT</div>
-          <div class="chamber-bot botShoot"></div>
+  <div class="stack__wrapper">
+    <div class="container">
+      <div class="stack">
+        <h1 class="stack__title">Russian roulette</h1>
+        <div class="stack__total-score-title">Total score</div>
+        <div class="stack__total-score">
+          <div class="bot-score">{{botScore}}</div>
+          <div>:</div>
+          <div class="user-score">{{userScore}}</div>
         </div>
-        <div class="stack__gun">
-          <div class="gun-shop">
-            <div class="chamber chamber-1" ref="1"></div>
-            <div class="chamber chamber-2" ref="2"></div>
-            <div class="chamber chamber-3" ref="3"></div>
-            <div class="chamber chamber-4" ref="4"></div>
-            <div class="chamber chamber-5" ref="5"></div>
-            <div class="chamber chamber-6" ref="6"></div>
-            <div class="ratchet"></div>
+        <div class="stack__content">
+          <div class="stack__player">
+            <div class="stack__player-title">BOT</div>
+            <input :checked="isBot" type="radio" name="player" id="bot">
+            <label for="bot"></label>
+          </div>
+          <div class="stack__gun">
+            <div class="gun-shop">
+              <div class="chamber chamber-1" ref="1"></div>
+              <div class="chamber chamber-2" ref="2"></div>
+              <div class="chamber chamber-3" ref="3"></div>
+              <div class="chamber chamber-4" ref="4"></div>
+              <div class="chamber chamber-5" ref="5"></div>
+              <div class="chamber chamber-6" ref="6"></div>
+              <div class="ratchet"></div>
+            </div>
+          </div>
+          <div class="stack__player">
+            <div class="stack__player-title">YOU</div>
+            <input :checked="isUser" type="radio" name="player" id="user">
+            <label for="user"></label>
           </div>
         </div>
-        <div class="stack__player">
-          <div class="stack__player-title">YOU</div>
-          <div class="chamber-player playerShoot"></div>
+        <div class="stack__buttons">
+          <div class="stack__button shoot" ref="shoot" @click="shootABullet">Shoot a bullet!</div>
+          <div class="stack__button refill" ref="refill" @click="refillAGun">Refill the gun shop</div>
         </div>
       </div>
-      <div class="stack__buttons">
-        <div class="stack__button shoot" ref="shoot" @click="shootABullet">Shoot a bullet!</div>
-        <div class="stack__button refill" ref="refill" @click="refillAGun">Refill the gun shop</div>
-      </div>
-    </div>
 
-    <div v-if="gameOver|| botGameOver" class="background"></div>
-    <div v-if="gameOver || botGameOver" class="score">
-      <h2 class="score__title" v-if="gameOver && !botGameOver">You lose!</h2>
-      <h2 class="score__title" v-if="!gameOver && botGameOver">You win!</h2>
-      <div class="score__content">
-        <div class="score__sub-title">Score</div>
-        <span class="score__bot">{{ +botGameOver }}</span> 
-        <span>:</span>
-        <span class="score__player">{{ +gameOver }}</span>
-      </div>
-      <div class="score__button">
-        <div @click="refillAGun()" class="score__restart">Restart</div>
+      <div v-if="gameOver|| botGameOver" class="background"></div>
+      <div v-if="gameOver || botGameOver" class="score">
+        <h2 class="score__title" v-if="gameOver && !botGameOver">You lose!</h2>
+        <h2 class="score__title" v-if="!gameOver && botGameOver">You win!</h2>
+        <div class="score__content">
+          <div class="score__sub-title">Game Score</div>
+          <span class="score__bot">{{ +botGameOver }}</span> 
+          <span>:</span>
+          <span class="score__player">{{ +gameOver }}</span>
+        </div>
+        <div class="score__button">
+          <div @click="refillAGun()" class="score__restart">Restart</div>
+        </div>
       </div>
     </div>
   </div>
@@ -53,24 +63,28 @@ export default {
       refill: [],
       gameOver: false,
       botGameOver: false,
-      isPlayerShoot: false,
+      isUser: true,
+      isBot: false,
+      userScore: 0,
+      botScore: 0,
     }
   },
   mounted() {
-    console.log(this.$refs)
     this.refillAGun();
-    
   },
   methods: {
     refillAGun: function(){
       this.gameOver = false;
       this.botGameOver = false;
+      this.isUser = true;
+      this.isBot = false;
+
       this.refill = [];
       this.refill.push(1, 2, 3, 4, 5, 6);
       this.refill.sort(() => Math.random() - 0.5);
       let values = Object.values(this.$refs);
       values.forEach(element => {
-        element.classList.remove('playerShoot', 'botShoot')
+        element.classList.remove('shootenChamber')
       });
       console.log(this.refill);
     },
@@ -78,14 +92,17 @@ export default {
       let bullet = this.refill.pop()
         if(bullet == 1){
           this.gameOver = true;
+          this.botScore += 1;
         } else {
-          console.log(bullet);
           let values = Object.values(this.$refs);
           let styleChamber = values[this.refill.length-1];
-          styleChamber.classList.add('playerShoot')
+          styleChamber.classList.add('shootenChamber')
 
           this.$refs.shoot.classList.add('disabled')
           this.$refs.refill.classList.add('disabled')
+
+          this.isUser = false;
+          this.isBot = true;
 
           setTimeout(this.shootABotBullet, 2000);
         }
@@ -94,16 +111,19 @@ export default {
       let bulletBot = this.refill.pop();
         if(bulletBot == 1){
           this.botGameOver = true;
+          this.userScore += 1;
           this.$refs.shoot.classList.remove('disabled')
           this.$refs.refill.classList.remove('disabled')
         } else {
-          console.log(bulletBot);
           let values = Object.values(this.$refs);
           let styleChamber = values[this.refill.length-1];
-          styleChamber.classList.add('botShoot')
+          styleChamber.classList.add('shootenChamber')
 
           this.$refs.shoot.classList.remove('disabled')
           this.$refs.refill.classList.remove('disabled')
+
+          this.isUser = true;
+          this.isBot = false;
         }
     }
   }
